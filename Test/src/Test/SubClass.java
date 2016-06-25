5eebd891c963f4c7ba28a7d4867de52b09be70ee
Anton Lazarets <antonlazarets@gmail.com>
2016-06-25 20:54:16 +0300

    3d init fields

--- Test/src/Test/SubClass.java
+++ Test/src/Test/SubClass.java
@@ -1,4 +1,13 @@
 package Test;
 
 public class SubClass {
+    int x;
+    int y;
+    int z;
+
+    public SubClass(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 }
